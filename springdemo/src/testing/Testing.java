9273853d83e6388fb782046a2de6e9afb745411a
shubham deokar <sdeokar13599@gamil.comgit config --global user.name  shubham>
2020-11-02 23:22:36 +0530

    spring programm

---
+++ springdemo/src/testing/Testing.java
@@ -0,0 +1,21 @@
+package testing;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import springdemo.Employee;
+
+public class Testing {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		ApplicationContext con=new ClassPathXmlApplicationContext("config.xml");
+
+		Employee emp= (Employee) con.getBean("e1");
+		
+		
+		emp.work();
+
+}
+}
